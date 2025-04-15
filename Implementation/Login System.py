7f3aaee9c583f35497f2e7f70a36af4648680a32
Samuel Molla <sambenmayu@gmail.com>
2025-04-14 23:18:16 -0700

    Setup main window For Login System

---
+++ Implementation/Login System.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox
+import subprocess
+
+# Setup main window
+root = tk.Tk()
+root.title("Login Form")
+root.geometry("1080x720")
+root.configure(bg='#333333')
